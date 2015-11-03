f201e83a17ca7eac5215d4568825064f0a593ede
Roman Fedorenkov <suffering123494@yandex.ru>
2015-11-03 16:09:15 +0300

    feat(idea): round function

--- IDEA/app/core/idea.js
+++ IDEA/app/core/idea.js
@@ -1,20 +1,60 @@
 import BitArray from 'node-bitarray';
 
+const MULL_CONST = Math.pow(2, 16) + 1;
+const ADD_CONST = Math.pow(2, 16);
 
 // const key = BitArray.fromBinary('1111010110').toJSON().reverse();
 
 
-const permutation = (input, scheme) => input.map((item, index, arr) => arr[scheme[index] - 1]);
+// const permutation = (input, scheme) => input.map((item, index, arr) => arr[scheme[index] - 1]);
 
 const shift = (input, shiftValue) => input.map((item, index, arr) => arr[(index + shiftValue) % arr.length]);
 
-const size = (input, scheme) => scheme.map((item) => input[item - 1]);
+// const size = (input, scheme) => scheme.map((item) => input[item - 1]);
 
 const key = Array.from(new Array(128), () => Math.floor(2 * Math.random()));
 
 const arrDivide = (input, blockSize) => Array.from(new Array(input.length / blockSize), (item, index) => input.slice(index * blockSize, index * blockSize + blockSize));
 
+const binExtend = (input, inputSize) => input.unshift(...Array.from(new Array(inputSize - input.length), () => 0));
+
 // const generateKeys = () =>
+
+const mullByModule = (val1, val2) => {
+  console.log(val1, val2);
+  console.log(`${BitArray.toNumber(val1.slice().reverse())}`);
+  console.log(`${BitArray.toNumber(val2.slice().reverse())}`);
+
+  const result = ((BitArray.toNumber(val1.reverse()) * BitArray.toNumber(val2.reverse())) % MULL_CONST);
+
+  console.log(`Result in func: ${result}`);
+
+  const resultBin = BitArray.factory(result).toJSON();
+  binExtend(resultBin, 16);
+
+  console.log(resultBin);
+
+  return resultBin;
+};
+
+
+const addByModule = (val1, val2) => {
+  console.log(val1, val2);
+  console.log(`${BitArray.toNumber(val1.slice().reverse())}`);
+  console.log(`${BitArray.toNumber(val2.slice().reverse())}`);
+
+  const result = ((BitArray.toNumber(val1.reverse()) + BitArray.toNumber(val2.reverse())) % ADD_CONST);
+
+  console.log(`Result in func: ${result}`);
+
+  const resultBin = BitArray.factory(result).toJSON();
+  binExtend(resultBin, 16);
+
+  console.log(resultBin);
+
+  return resultBin;
+};
+
 
 function generateKeys() {
   const roundKeys = {};
@@ -30,8 +70,32 @@ function generateKeys() {
 
 function round(block, roundKeys, roundIndex) {
   const roundBlocks = arrDivide(block, 16);
-  roundBlocks[0] = BitArray.and(roundBlocks[0], roundKeys[roundIndex][0]);
-  roundBlocks[3] = BitArray.and(roundBlocks[3], roundKeys[roundIndex][3]);
+
+  console.log(`1: ${roundBlocks[0]}`);
+  console.log(`2: ${roundKeys[roundIndex][0]}`);
+
+  roundBlocks[0] = mullByModule(roundBlocks[0], roundKeys[roundIndex][0]);
+  roundBlocks[3] = mullByModule(roundBlocks[3], roundKeys[roundIndex][3]);
+  roundBlocks[1] = addByModule(roundBlocks[1], roundKeys[roundIndex][1]);
+  roundBlocks[2] = addByModule(roundBlocks[2], roundKeys[roundIndex][2]);
+
+  let tmpRoundBlock1 = BitArray.xor(roundBlocks[0], roundBlocks[2], BitArray.factory(0, 16));
+  let tmpRoundBlock2 = BitArray.xor(roundBlocks[1], roundBlocks[3], BitArray.factory(0, 16));
+
+  tmpRoundBlock1 = mullByModule(tmpRoundBlock1, roundKeys[roundIndex][4]);
+  tmpRoundBlock2 = addByModule(tmpRoundBlock1, tmpRoundBlock2);
+
+  tmpRoundBlock2 = mullByModule(tmpRoundBlock2, roundKeys[roundIndex][5]);
+  tmpRoundBlock1 = addByModule(tmpRoundBlock1, tmpRoundBlock2);
+
+  roundBlocks[0] = mullByModule(roundBlocks[0], tmpRoundBlock2);
+  roundBlocks[1] = mullByModule(roundBlocks[1], tmpRoundBlock1);
+  roundBlocks[2] = addByModule(roundBlocks[2], tmpRoundBlock2);
+  roundBlocks[3] = addByModule(roundBlocks[3], tmpRoundBlock1);
+
+  [roundBlocks[1], roundBlocks[2]] = [roundBlocks[2], roundBlocks[1]];
+
+  return roundBlocks;
 }
 
 
